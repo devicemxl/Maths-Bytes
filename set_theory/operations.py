a499616f44771fdd1dd53961bf895e7c2470a1c1
David  Ochoa Corrales <david.ochoa.corrales@gmail.com>
2022-01-19 20:50:19 -0600

    Update operations.py
    Add Intersection Routine

--- set_theory/operations.py
+++ set_theory/operations.py
@@ -1,6 +1,10 @@
+# Set Theory:
+#
+# Intersection Routine
+def intersection(a,b): c = set(a).intersection(set(b));return c
+#
 # Set Theory: Cartesian Product Routine
 def CartesianProduct(a,b,c):
     for C in range(len(b)):
-        for D in range(len(a)):
-            c.append(tuple(b[C]+a[D]))
-    return c
+        for D in range(len(a)): c.append(tuple(b[C]+a[D])); return c
+#
