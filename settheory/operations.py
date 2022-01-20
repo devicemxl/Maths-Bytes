00c7367062659605b47cf8f2399dd2f08799c646
David  Ochoa Corrales <david.ochoa.corrales@gmail.com>
2022-01-19 19:54:20 -0600

    Update operations.py
    add Cartesian Product Routine

--- settheory/operations.py
+++ settheory/operations.py
@@ -1,1 +1,7 @@
 
+# Set Theory: Cartesian Product Routine
+def CartesianProduct(a,b,c):
+    for C in range(len(b)):
+        for D in range(len(a)):
+            c.append(tuple(b[C]+a[D]))
+    return c
